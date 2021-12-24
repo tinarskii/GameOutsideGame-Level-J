9b61d9ef3a6dd6c202cbbfa6222967581e080a01
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2021-12-24 20:29:01 +0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const num = '0110100100100000001011010100010100100101111000100000010001010000110010011010111101110001';
+const nums = num.match(/.{1,4}/g);
+
+const tree = ' wEv1nToLhauSmcd'.split('')
+
+let numberArr = [];
+let treeArr = [];
+
+nums.forEach((numbers) => {
+    const number = parseInt(numbers, 2);
+    numberArr.push(number);
+})
+
+if (numberArr.length === nums.length) {
+    numberArr.forEach(numbers => {
+        treeArr.push(tree[numbers]);
+        
+    })
+}
+
+if (treeArr.length === numberArr.length) {
+    console.log(treeArr.join(''));
+}
